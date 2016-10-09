55d41fbc453d8dff72827295fab7b275fa2b2f45
Ewan Sheldon <ewansheldon@gmail.com>
2016-10-09 20:01:58 +0100

    photos view and link to i indvidual photo

---
+++ ewanstagram/app/models/photo.rb
@@ -0,0 +1,6 @@
+class Photo < ApplicationRecord
+
+  has_attached_file :image, :styles => { :medium => "200x200>"}
+
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+end
--- ewanstagram/spec/features/photos_feature_spec.rb
+++ ewanstagram/spec/features/photos_feature_spec.rb
@@ -8,4 +8,27 @@ feature 'photos' do
       expect(page).to have_link 'Add a photo'
     end
   end
+
+    context 'photos have been added' do
+    before do
+      Photo.create(caption: 'first photo')
+    end
+
+    scenario 'display restaurants' do
+      visit '/photos'
+      expect(page).to have_content('first photo')
+      expect(page).not_to have_content('No photos yet!')
+    end
+  end
+
+  context 'posting photos' do
+    scenario 'prompts a user to fill out form for caption and then displays photo' do
+      visit '/photos'
+      click_link 'Add a photo'
+      fill_in 'photo_caption', with: 'second photo'
+      click_button 'Post photo'
+      expect(page).to have_content 'second photo'
+      expect(current_path).to eq '/'
+    end
+  end
 end
